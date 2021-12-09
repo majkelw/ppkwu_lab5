b6efa66b2c00165f854b06712ede39e794de19ff
majkelw <michalwik@vp.pl>
2021-12-09 13:26:13 +0100

    Refactoring, + css

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from bs4 import BeautifulSoup
-import jinja2
+from pathlib import Path
+
 import uvicorn
 import requests
 import json
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/api/search/{employee}", response_class=HTMLResponse)
 def find_employees(request: Request, employee):
-    data = []
     url = "https://panoramafirm.pl/{}".format(employee)
+    data = extract_data(url)
+    templates = Jinja2Templates(directory="templates")
+    return templates.TemplateResponse("exmployees.html", {"request": request, "data": data})
+
+
+def extract_data(url):
+    data = []
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     result = soup.select("script[type=\"application/ld+json\"]")
     for r in result:
         data.append(json.loads(r.contents[0]))
-    templates = Jinja2Templates(directory="../templates")
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    return data
 
 
 if __name__ == '__main__':
