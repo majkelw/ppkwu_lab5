64f9c3d4f273fab9dc1c71e5ac661566d5908a32
majkelw <michalwik@vp.pl>
2021-12-09 14:01:37 +0100

    Added vcard generation

--- src/main.py
+++ src/main.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from bs4 import BeautifulSoup
-from pathlib import Path
-
+import vobject
 import uvicorn
 import requests
 import json
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 @app.get("/api/search/{employee}", response_class=HTMLResponse)
 def find_employees(request: Request, employee):
     url = "https://panoramafirm.pl/{}".format(employee)
     data = extract_data(url)
-    templates = Jinja2Templates(directory="templates")
+    templates = Jinja2Templates(directory="../templates")
     return templates.TemplateResponse("exmployees.html", {"request": request, "data": data})
+
+
+@app.post("/api/vcard", response_class=FileResponse)
+def create_vcard(name: str = Form(...), email: str = Form(...), telephone: str = Form(...)):
+    v = vobject.vCard()
+    print(name)
+    print(email)
+    v.add('fn').value = name
+    v.add('email').value = email
+    v.add('tel').value = telephone
+    return HTMLResponse(content=v.serialize(),
+                        headers={"Content-Type": "text/x-vcard",
+                                 "Content-Disposition": "attachment; filename=\"VCARD.VCF\""})
 
 
 def extract_data(url):
