3d8db9ccdee0027c474aa43cf4e4d1306d4a6c48
majkelw <michalwik@vp.pl>
2021-12-09 14:34:52 +0100

    Added addresses

--- src/main.py
+++ src/main.py
@@ -15,20 +15,22 @@ def find_employees(request: Request, employee):
     url = "https://panoramafirm.pl/{}".format(employee)
     data = extract_data(url)
     templates = Jinja2Templates(directory="../templates")
+    print(data)
     return templates.TemplateResponse("exmployees.html", {"request": request, "data": data})
 
 
 @app.post("/api/vcard", response_class=FileResponse)
-def create_vcard(name: str = Form(...), email: str = Form(...), telephone: str = Form(...)):
+async def create_vcard(name: str = Form(...), email: str = Form(...), telephone: str = Form(...)):
     v = vobject.vCard()
     print(name)
     print(email)
+    print(telephone)
     v.add('fn').value = name
     v.add('email').value = email
     v.add('tel').value = telephone
     return HTMLResponse(content=v.serialize(),
                         headers={"Content-Type": "text/x-vcard",
-                                 "Content-Disposition": "attachment; filename=\"VCARD.VCF\""})
+                                 "Content-Disposition": "attachment; filename=\"vcard.vcf\""})
 
 
 def extract_data(url):
@@ -36,9 +38,19 @@ def extract_data(url):
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     result = soup.select("script[type=\"application/ld+json\"]")
     for r in result:
-        data.append(json.loads(r.contents[0]))
+        print('its one')
+        one = json.loads(r.contents[0])
+        print(one)
+        if "address" in one:
+            print(one["address"]["addressLocality"])
+        else:
+            one["address"] = "Adres"
+        one.update({'fullAddress': 'value'})
+        print(one)
+        print('end')
+        data.append(one)
     return data
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="localhost", port=8080)
+    uvicorn.run(app, host="192.168.1.20", port=8080)
