e711f1fbdb194223a0d5458f24284281a97155e1
majkelw <michalwik@vp.pl>
2021-12-09 12:51:06 +0100

    Added script to find specified employee from website

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
 import uvicorn
+import requests
+import json
 
 app = FastAPI()
 
 
-@app.get("/api/test")
-def hello():
-    return {"test": "test"}
+@app.get("/api/search/{employee}")
+def find_employees(employee):
+    data = []
+    url = "https://panoramafirm.pl/{}".format(employee)
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    result = soup.select("script[type=\"application/ld+json\"]")
+    for r in result:
+        data.append(json.loads(r.contents[0]))
+    return data
 
 
 if __name__ == '__main__':
