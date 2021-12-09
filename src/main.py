0214c6b066acc541c18ac820f2c5e5f420947c05
majkelw <michalwik@vp.pl>
2021-12-09 13:04:39 +0100

    Added example template with employees

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from bs4 import BeautifulSoup
+import jinja2
 import uvicorn
 import requests
 import json
@@ -7,15 +10,16 @@ import json
 app = FastAPI()
 
 
-@app.get("/api/search/{employee}")
-def find_employees(employee):
+@app.get("/api/search/{employee}", response_class=HTMLResponse)
+def find_employees(request: Request, employee):
     data = []
     url = "https://panoramafirm.pl/{}".format(employee)
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     result = soup.select("script[type=\"application/ld+json\"]")
     for r in result:
         data.append(json.loads(r.contents[0]))
-    return data
+    templates = Jinja2Templates(directory="../templates")
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
 
 
 if __name__ == '__main__':
